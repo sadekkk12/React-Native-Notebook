4758f8d7a69463e5ff139944b651f80384e02a80
sadekkk12 <sade0027@stud.kea.dk>
2024-02-21 22:23:59 +0100

    Navigation added

--- App.js
+++ App.js
@@ -1,11 +1,64 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, FlatList, Text, View, Image, Button, SafeAreaView, TextInput, TouchableOpacity} from 'react-native';
+import { NavigationContainer} from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+const Stack = createNativeStackNavigator();
 export default function App() {
   return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="ListPage">
+        <Stack.Screen name="ListPage" component={ListPage} />
+        <Stack.Screen name="DetailPage" component={DetailPage} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const ListPage = ({ navigation }) => {
+  const [text, setText] = useState('');
+  const [notes, setNotes] = useState([]);
+
+  function buttonPressed() {
+    setNotes([...notes, { key: notes.length.toString(), name: text }]);
+    setText(''); 
+  }
+  return (
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>
+        <TextInput 
+          style={styles.textField} 
+          onChangeText={setText} 
+          value={text}
+          placeholder="Add a word" 
+          placeholderTextColor="#999" 
+        />
+        <Button title="Press Me" onPress={buttonPressed} />
+        <FlatList 
+  data={notes}
+  renderItem={({ item }) => (
+    <TouchableOpacity 
+      style={styles.button} 
+      onPress={() => navigation.navigate('DetailPage', { text: item.name })}
+    >
+      <Text style={styles.buttonText}>{item.name.substring(0, 30)}</Text>
+    </TouchableOpacity>
+  )}
+  keyExtractor={item => item.key.toString()} 
+/>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const DetailPage = ({ navigation, route }) => {
+  const {text} = route.params;
+  
+  return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>{text}</Text>
+    
     </View>
   );
 }
@@ -13,8 +66,39 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#191C27',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  
+  card: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 50,
+    paddingLeft: 150,
+    paddingRight: 150,
+    borderRadius: 10,
+    backgroundColor: '#191C27',
+  },
+  text: {
+    marginTop: 20,
+    fontSize: 16,
+    color: "#fff",
+    margin: 5,
+  },
+  safeArea: {
+    flex: 1,
+    backgroundColor: '#191C27', 
+  },
+  textField: {
+    backgroundColor: '#fff',
+    minWidth: 200,
+    marginTop: '50%',
+
+
+
+    },
+    buttonText: {
+      color: 'white',
+    },
 });
