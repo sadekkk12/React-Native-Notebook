c7cbb97ad983be61c1f7ac0dabab7362a5d29191
sadekkk12 <sade0027@stud.kea.dk>
2024-04-10 14:54:46 +0200

    FireBase working again

--- firebase.js
+++ firebase.js
@@ -1,17 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore} from 'firebase/firestore'
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA9qvB0945cJFa-CUhGo-ZPuBYzSbQoWKw",
-  authDomain: "notebook-9f484.firebaseapp.com",
-  projectId: "notebook-9f484",
-  storageBucket: "notebook-9f484.appspot.com",
-  messagingSenderId: "898915425606",
-  appId: "1:898915425606:web:58721eb28e6486e3135947"
+  apiKey: "AIzaSyBSfjbhmAy-1koiTv4ClhgKYYfqDSjYQDM",
+  authDomain: "notebook-6b8df.firebaseapp.com",
+  projectId: "notebook-6b8df",
+  storageBucket: "notebook-6b8df.appspot.com",
+  messagingSenderId: "681900691658",
+  appId: "1:681900691658:web:04fb6135b3f5d4948b2433"
 };
 
 // Initialize Firebase
