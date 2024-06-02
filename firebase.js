5de385ee1f83fc32cda52d04a74d733f43f84fbc
Sadek <sade0027@stud.kea.dk>
2024-06-02 11:34:56 +0200

    Uploading and downlading image from firebase + Camera

--- firebase.js
+++ firebase.js
@@ -1,20 +1,22 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBSfjbhmAy-1koiTv4ClhgKYYfqDSjYQDM",
-  authDomain: "notebook-6b8df.firebaseapp.com",
-  projectId: "notebook-6b8df",
-  storageBucket: "notebook-6b8df.appspot.com",
-  messagingSenderId: "681900691658",
-  appId: "1:681900691658:web:04fb6135b3f5d4948b2433"
+  apiKey: "AIzaSyBY4vzPHEBq2SjMrc8B0twMgae_SW6hkxA",
+  authDomain: "noteeaa-b2311.firebaseapp.com",
+  projectId: "noteeaa-b2311",
+  storageBucket: "noteeaa-b2311.appspot.com",
+  messagingSenderId: "492677674566",
+  appId: "1:492677674566:web:3a416aacf32a6cf6e63c47",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const database = getFirestore(app)
-export {app, database}
+const database = getFirestore(app);
+const storage = getStorage(app);
+export { app, database, storage };
